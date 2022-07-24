9480517618d4515a0088db6218e123f2d426abff
ethanelliott <ethan.elliott@hotmail.ca>
2022-07-24 19:23:23 -0400

    add log rocket

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import vuetify from "./plugins/vuetify";
 import { apolloProvider } from "@/apollo/apollo-provider";
 import VueApollo from "vue-apollo";
 import { env } from "@/env";
+import LogRocket from "logrocket";
 
 Vue.config.productionTip = false;
 
@@ -16,6 +17,8 @@ Vue.prototype.$frontUrl = env.frontUrl;
 Vue.prototype.$graphqlUrl = env.graphqlUrl;
 
 Vue.use(VueApollo);
+
+LogRocket.init("ayfxcp/upnext");
 
 new Vue({
   router,
